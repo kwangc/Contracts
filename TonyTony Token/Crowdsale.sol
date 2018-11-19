9c8755fe7ea8505604df2ff63c8781617c6684d3
Tony Lee <lkhsoft@gmail.com>
2018-11-19 15:23:25 +0900

    Uploading initial contracts

---
+++ TonyTony Token/Crowdsale.sol
@@ -0,0 +1,295 @@
+pragma solidity ^0.4.25;
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+    return c;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address who) external view returns (uint256);
+
+  function allowance(address owner, address spender)
+    external view returns (uint256);
+
+  function transfer(address to, uint256 value) external returns (bool);
+
+  function approve(address spender, uint256 value)
+    external returns (bool);
+
+  function transferFrom(address from, address to, uint256 value)
+    external returns (bool);
+
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+  );
+
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+
+library SafeERC20 {
+  function safeTransfer(
+    IERC20 token,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transfer(to, value));
+  }
+
+  function safeTransferFrom(
+    IERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transferFrom(from, to, value));
+  }
+
+  function safeApprove(
+    IERC20 token,
+    address spender,
+    uint256 value
+  )
+    internal
+  {
+    require(token.approve(spender, value));
+  }
+}
+
+contract Ownable {
+  address private _owner;
+
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  constructor() public {
+    _owner = msg.sender;
+  }
+
+  function owner() public view returns(address) {
+    return _owner;
+  }
+
+  modifier onlyOwner() {
+    require(isOwner());
+    _;
+  }
+
+  function isOwner() public view returns(bool) {
+    return msg.sender == _owner;
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    _transferOwnership(newOwner);
+  }
+
+  function _transferOwnership(address newOwner) internal {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(_owner, newOwner);
+    _owner = newOwner;
+  }
+}
+
+contract Crowdsale is Ownable {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  // The token being sold
+  IERC20 private _token;
+  // Address where funds are collected
+  address private _wallet;
+  // How many token units a buyer gets per wei.
+  // The rate is the conversion between wei and the smallest and indivisible token unit.
+  // So, if you are using a rate of 1 with a ERC20Detailed token with 3 decimals called TOK
+  // 1 wei will give you 1 unit, or 0.001 TOK.
+  uint256 private _rate;
+  // Amount of wei raised
+  uint256 private _weiRaised;
+
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokensPurchased(
+    address indexed purchaser,
+    address indexed beneficiary,
+    uint256 value,
+    uint256 amount
+  );
+
+  /**
+   * @param rate Number of token units a buyer gets per wei
+   * @dev The rate is the conversion between wei and the smallest and indivisible
+   * token unit. So, if you are using a rate of 1 with a ERC20Detailed token
+   * with 3 decimals called TOK, 1 wei will give you 1 unit, or 0.001 TOK.
+   * @param wallet Address where collected funds will be forwarded to
+   * @param token Address of the token being sold
+   */
+  constructor(uint256 rate, address wallet, IERC20 token) public {
+    require(rate > 0);
+    require(wallet != address(0));
+    require(token != address(0));
+
+    _rate = rate;
+    _wallet = wallet;
+    _token = token;
+  }
+
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  function token() public view returns(IERC20) {
+    return _token;
+  }
+
+  function wallet() public view returns(address) {
+    return _wallet;
+  }
+
+  function rate() public view returns(uint256) {
+    return _rate;
+  }
+
+  function weiRaised() public view returns (uint256) {
+    return _weiRaised;
+  }
+
+  function buyTokens(address beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(beneficiary, weiAmount);
+    uint256 tokens = _getTokenAmount(weiAmount);
+    _weiRaised = _weiRaised.add(weiAmount);
+
+    _processPurchase(beneficiary, tokens);
+    emit TokensPurchased(
+      msg.sender,
+      beneficiary,
+      weiAmount,
+      tokens
+    );
+
+    _updatePurchasingState(beneficiary, weiAmount);
+
+    _forwardFunds();
+    _postValidatePurchase(beneficiary, weiAmount);
+  }
+
+  function _preValidatePurchase(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+  {
+    require(beneficiary != address(0));
+    require(weiAmount != 0);
+  }
+
+  function _postValidatePurchase(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+  {
+  }
+
+  function _deliverTokens(
+    address beneficiary,
+    uint256 tokenAmount
+  )
+    internal
+  {
+    _token.safeTransfer(beneficiary, tokenAmount);
+  }
+
+  function _processPurchase(
+    address beneficiary,
+    uint256 tokenAmount
+  )
+    internal
+  {
+    _deliverTokens(beneficiary, tokenAmount);
+  }
+
+  function _updatePurchasingState(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+  {
+    // optional override
+  }
+
+  function _getTokenAmount(uint256 weiAmount)
+    internal view returns (uint256)
+  {
+    return weiAmount.mul(_rate);
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    _wallet.transfer(msg.value);
+  }
+}
+
+contract CanReclaimToken is Ownable {
+  using SafeERC20 for IERC20;
+
+  /**
+   * @dev Reclaim all ERC20 compatible tokens
+   * @param _token ERC20 The address of the token contract
+   */
+  function reclaimToken(IERC20 _token) external onlyOwner {
+    uint256 balance = _token.balanceOf(this);
+    _token.safeTransfer(owner(), balance);
+  }
+}
---
+++ TonyTony Token/TonyTony Token.sol
@@ -0,0 +1,452 @@
+pragma solidity ^0.4.25;
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+    return c;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+
+library Roles {
+  struct Role {
+    mapping (address => bool) bearer;
+  }
+
+  function add(Role storage role, address account) internal {
+    require(account != address(0));
+    role.bearer[account] = true;
+  }
+
+  function remove(Role storage role, address account) internal {
+    require(account != address(0));
+    role.bearer[account] = false;
+  }
+
+  function has(Role storage role, address account)
+    internal
+    view
+    returns (bool)
+  {
+    require(account != address(0));
+    return role.bearer[account];
+  }
+}
+
+contract Ownable {
+  address private _owner;
+
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  constructor() public {
+    _owner = msg.sender;
+  }
+
+  function owner() public view returns(address) {
+    return _owner;
+  }
+
+  modifier onlyOwner() {
+    require(isOwner());
+    _;
+  }
+
+  function isOwner() public view returns(bool) {
+    return msg.sender == _owner;
+  }
+
+  function transferOwnership(address newOwner) public onlyOwner {
+    _transferOwnership(newOwner);
+  }
+
+  function _transferOwnership(address newOwner) internal {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(_owner, newOwner);
+    _owner = newOwner;
+  }
+}
+
+contract PauserRole {
+  using Roles for Roles.Role;
+
+  event PauserAdded(address indexed account);
+  event PauserRemoved(address indexed account);
+
+  Roles.Role private pausers;
+
+  constructor() public {
+    _addPauser(msg.sender);
+  }
+
+  modifier onlyPauser() {
+    require(isPauser(msg.sender));
+    _;
+  }
+
+  function isPauser(address account) public view returns (bool) {
+    return pausers.has(account);
+  }
+
+  function addPauser(address account) public onlyPauser {
+    _addPauser(account);
+  }
+
+  function renouncePauser() public {
+    _removePauser(msg.sender);
+  }
+
+  function _addPauser(address account) internal {
+    pausers.add(account);
+    emit PauserAdded(account);
+  }
+
+  function _removePauser(address account) internal {
+    pausers.remove(account);
+    emit PauserRemoved(account);
+  }
+}
+
+contract Pausable is PauserRole {
+  event Paused();
+  event Unpaused();
+
+  bool private _paused = false;
+
+  function paused() public view returns(bool) {
+    return _paused;
+  }
+
+  modifier whenNotPaused() {
+    require(!_paused);
+    _;
+  }
+
+  modifier whenPaused() {
+    require(_paused);
+    _;
+  }
+
+  function pause() public onlyPauser whenNotPaused {
+    _paused = true;
+    emit Paused();
+  }
+
+  function unpause() public onlyPauser whenPaused {
+    _paused = false;
+    emit Unpaused();
+  }
+}
+
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address who) external view returns (uint256);
+
+  function allowance(address owner, address spender)
+    external view returns (uint256);
+
+  function transfer(address to, uint256 value) external returns (bool);
+
+  function approve(address spender, uint256 value)
+    external returns (bool);
+
+  function transferFrom(address from, address to, uint256 value)
+    external returns (bool);
+
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+  );
+
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+
+  event FrozenFunds(
+    address account,
+    bool frozen
+  );
+}
+
+contract ERC20 is IERC20 {
+  using SafeMath for uint256;
+
+  mapping (address => uint256) private _balances;
+  mapping (address => mapping (address => uint256)) private _allowed;
+  mapping (address => bool) public frozenAccount;
+
+  uint256 private _totalSupply;
+
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address owner) public view returns (uint256) {
+    return _balances[owner];
+  }
+
+  function allowance(
+    address owner,
+    address spender
+   )
+    public
+    view
+    returns (uint256)
+  {
+    return _allowed[owner][spender];
+  }
+
+  function transfer(address to, uint256 value) public returns (bool) {
+    require(value <= _balances[msg.sender]);
+    require(to != address(0));
+    require(!frozenAccount[msg.sender]);
+    require(!frozenAccount[to]);
+
+    _balances[msg.sender] = _balances[msg.sender].sub(value);
+    _balances[to] = _balances[to].add(value);
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function approve(address spender, uint256 value) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  )
+    public
+    returns (bool)
+  {
+    require(value <= _balances[from]);
+    require(value <= _allowed[from][msg.sender]);
+    require(to != address(0));
+
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+    emit Transfer(from, to, value);
+    return true;
+  }
+
+  function increaseAllowance(
+    address spender,
+    uint256 addedValue
+  )
+    public
+    returns (bool)
+  {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].add(addedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  function decreaseAllowance(
+    address spender,
+    uint256 subtractedValue
+  )
+    public
+    returns (bool)
+  {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].sub(subtractedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  function _mint(address account, uint256 amount) internal {
+    require(account != 0);
+    _totalSupply = _totalSupply.add(amount);
+    _balances[account] = _balances[account].add(amount);
+    emit Transfer(address(0), account, amount);
+  }
+
+  function _burn(address account, uint256 amount) internal {
+    require(account != 0);
+    require(amount <= _balances[account]);
+
+    _totalSupply = _totalSupply.sub(amount);
+    _balances[account] = _balances[account].sub(amount);
+    emit Transfer(account, address(0), amount);
+  }
+
+  function _burnFrom(address account, uint256 amount) internal {
+    require(amount <= _allowed[account][msg.sender]);
+
+    _allowed[account][msg.sender] = _allowed[account][msg.sender].sub(
+      amount);
+    _burn(account, amount);
+  }
+}
+
+contract TonyTonyToken is ERC20, Ownable, Pausable {
+
+  string public constant name = "TonyTony Token";
+  string public constant symbol = "TTT";
+  uint8 public constant decimals = 18;
+
+  uint256 public constant INITIAL_SUPPLY = 10 * 10**8 * (10 ** uint256(decimals)); //1B
+
+  constructor(address InitOwner) public {
+    _mint(InitOwner, INITIAL_SUPPLY);
+  }
+
+  function transfer(
+    address to,
+    uint256 value
+  )
+    public
+    whenNotPaused
+    returns (bool)
+  {
+    return super.transfer(to, value);
+  }
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  )
+    public
+    whenNotPaused
+    returns (bool)
+  {
+    return super.transferFrom(from, to, value);
+  }
+
+  function approve(
+    address spender,
+    uint256 value
+  )
+    public
+    whenNotPaused
+    returns (bool)
+  {
+    return super.approve(spender, value);
+  }
+
+  function increaseAllowance(
+    address spender,
+    uint addedValue
+  )
+    public
+    whenNotPaused
+    returns (bool success)
+  {
+    return super.increaseAllowance(spender, addedValue);
+  }
+
+  function decreaseAllowance(
+    address spender,
+    uint subtractedValue
+  )
+    public
+    whenNotPaused
+    returns (bool success)
+  {
+    return super.decreaseAllowance(spender, subtractedValue);
+  }
+
+  function burn(uint256 value) public {
+    _burn(msg.sender, value);
+  }
+
+  function burnFrom(address from, uint256 value) public {
+    _burnFrom(from, value);
+  }
+
+  function _burn(address who, uint256 value) internal {
+    super._burn(who, value);
+  }
+
+  function freezeAccount(address account, bool freeze) onlyOwner public {
+      frozenAccount[account] = freeze;
+      emit FrozenFunds(account, freeze);
+  }
+}
+
+library SafeERC20 {
+  function safeTransfer(
+    IERC20 token,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transfer(to, value));
+  }
+
+  function safeTransferFrom(
+    IERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transferFrom(from, to, value));
+  }
+
+  function safeApprove(
+    IERC20 token,
+    address spender,
+    uint256 value
+  )
+    internal
+  {
+    require(token.approve(spender, value));
+  }
+}
+
+contract CanReclaimToken is Ownable {
+  using SafeERC20 for IERC20;
+
+  function reclaimToken(IERC20 _token) external onlyOwner {
+    uint256 balance = _token.balanceOf(this);
+    _token.safeTransfer(owner(), balance);
+  }
+}
